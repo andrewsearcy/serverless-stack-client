30779c082a7cc4650a87b2a8c9a15c75d87354a1
andrewsearcy <andrew.searcy30@gmail.com>
2020-12-07 22:42:44 -0600

    Changes to notes client. auth, routes, settings, billing etc.

--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
 const config = {
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "my.note.app.uploads",
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: " https://ogyd46wv0f.execute-api.us-east-1.amazonaws.com/prod",
-  },
-  cognito: {
-    REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_YrhZZ3hPQ",
-    APP_CLIENT_ID: "5a3dlm3jcokp31ehldn9ddjo55",
-    IDENTITY_POOL_ID: "andrews-notes-user-pool",
-  },
+    STRIPE_KEY: "pk_test_51HjzdiJ5PpiL9fg2rgy4DcAzrZQYN8Z3oe5S7Fp6ox7O0TpCxXO2vXW0MAo5TbRbwilCKWxTqR5ut9VgHbwFZRoF00xvHyLeIX",
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "my.note.app.uploads",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://ogyd46wv0f.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_YrhZZ3hPQ",
+        APP_CLIENT_ID: "5a3dlm3jcokp31ehldn9ddjo55",
+        IDENTITY_POOL_ID: "us-east-2:d6500249-bcf4-42ab-b97c-52d92221ea49",
+    },
 };
 
 export default config;
